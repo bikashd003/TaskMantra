c78f28c3df32dbb32ec4bddfbd1742d5e1c86148
Bikash Das <bikashd003@gmail.com>
2025-01-28 17:12:35 +0530

    bug fix for deployment again

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,7 +2,6 @@ import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import { logger } from 'hono/logger'
 
-
 const app = new Hono().basePath('/api')
 
 app.use('*', logger())
@@ -11,10 +10,9 @@ const createTask = async (c: any) => {
     return c.json({ message: 'Task created successfully' })
 }
 
-const routes = app
-    .route('/create-task', createTask)
+app.post('/create-task', createTask)
 
-export const GET = handle(routes)
-export const POST = handle(routes)
-export const DELETE = handle(routes)
-export const PUT = handle(routes)
+export const GET = handle(app)
+export const POST = handle(app)
+export const DELETE = handle(app)
+export const PUT = handle(app)
