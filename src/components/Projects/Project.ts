93273f4dcb5b31a0f99c98d73c494f2905cb6e72
Bikash Das <bikashd003@gmail.com>
2025-01-28 16:52:40 +0530

    bug fixed

--- src/components/Projects/Project.ts
+++ src/components/Projects/Project.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const projectManagementData = {
+export const projectManagementData = {
   projects: [
     {
       id: 1,
--- src/components/Settings/settings-layout.tsx
+++ src/components/Settings/settings-layout.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import { SidebarNav } from "./sidebar-nav"
 
 interface SettingsLayoutProps {
