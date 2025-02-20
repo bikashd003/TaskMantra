e06f297952720d2451b2c0a57629da5e53a08d91
Bikash Das <bikashd003@gmail.com>
2025-02-21 01:10:01 +0530

    bugfix

--- src/components/IndividualProject/ProjectAnalytics.tsx
+++ src/components/IndividualProject/ProjectAnalytics.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ProjectAnalytics = ({ project }: { project: any }) => {
+const ProjectAnalytics = ({ _project }: { project: any }) => {
   return (
     <div className='bg-green-300'>ProjectAnalytics</div>
   )
