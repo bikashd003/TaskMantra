101aa6a50bec275d3c09afa8d6c995afdc603641
Bikash Das <bikashd003@gmail.com>
2025-04-26 01:01:17 +0530

    bug fix

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import SignIn from '@/components/auth/SignIn';
 import SignUp from '@/components/auth/SignUp';
